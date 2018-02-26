26ba0e263ee9dbdd844b4618b7f0d69ab4e2a757
Nikhail Singh <singhn18@mills.cas.mcmaster.ca>
2018-02-26 00:01:27 -0500

    modified ProjectAnalyze started to implement function4

--- ProjectAnalyze.sh
+++ ProjectAnalyze.sh
@@ -4,4 +4,11 @@ git status
 
 git diff HEAD --  . ':(exclude)changes.log' > "changes.log"
 
-grep -r "#TODO" > "todo.log"
+grep -r --exclude={todo.log,changes.log,ProjectAnalyze.sh} "#TODO" > "todo.log"
+
+find . -name "*.hs" -print0 |
+	while IFS=’’ read -r -d $’\0’ file
+	do
+		ghc -fno-code file.hs 2> error.log 
+	done
+
